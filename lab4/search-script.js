1ba9c03bddf5c14c957f8a6fb16381a207e00f4f
MisterRnobe <mr.rnobe@gmail.com>
2018-10-16 17:46:23 +0300

    Commit

--- lab4/search-script.js
+++ lab4/search-script.js
@@ -10,7 +10,24 @@ const search = [
     {
         name: 'Mail.ru',
         url: 'http://go.mail.ru/search?fm=1&rf=e.mail.ru&q='
-    }
+    },{name: 'DuckDuckGo', url : 'https://duckduckgo.com/?q='},
+    { name : 'Yahoo', url :'https://search.yahoo.com/search?p='},
+    { name :
+            'Bing', url :'http://www.bing.com/search?q='},
+    { name :
+            'Yahoo', url :'https://search.yahoo.com/search?p='},
+    { name :
+            'Rambler', url :'https://nova.rambler.ru/search?query='},
+    { name :
+            'Wikipedia', url :'https://ru.wikipedia.org/w/index.php?search='},{ name :
+            'Aport', url :'https://www.aport.ru/search/?q='},{ name :
+            'Search', url :'https://www.search.com/web?q='},{ name :
+            'Clarivate', url :'https://clarivate.com/?s='},{ name :
+            'Webcrawler', url :'http://www.webcrawler.com/serp?q='},{ name :
+            'IRR', url :'https://irr.ru/searchads/search/keywords='},{ name :
+            'Price', url :'https://price.ru/search/?query='},{ name :
+            'Ask', url :'https://www.ask.com/web?q='}
+
 ];
 function setList() {
     const select = $('#search-select');
